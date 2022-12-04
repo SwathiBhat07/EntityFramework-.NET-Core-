eb5821eaef40769f28d97cf220758e48c799ab79
Swathi Bhat <bhatswathi2221@gmail.com>
2022-12-04 22:57:36 +0530

    Add project files.

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }//this will create the category table in database.
+    }
+}
---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key] // this tells the sql to make the Id as the primary key and identity column in Database
+        public int Id { get; set; }  //Property 
+        [Required]// this tells the sql that name is required property and it should not be null.
+        public String Name { get; set; }
+        public int DisplaySeq { get; set; }
+        public DateTime Createddatetime { get; set; } = DateTime.Now;
+
+    }
+}
