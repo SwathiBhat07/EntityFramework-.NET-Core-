282ee02eee69a80af0c4d982501ef9925a7e4773
Swathi Bhat <bhatswathi2221@gmail.com>
2022-12-10 15:46:09 +0530

    added DBContext service to reterive all the Categories form the Database

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db; 
+
+        public CategoryController(ApplicationDbContext db) //this will retrieve the Db contents
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> CategoryList = _db.Categories; //Used Ienumerable to display all the categories
+            return View(CategoryList);
+
+          
         }
     }
 }
