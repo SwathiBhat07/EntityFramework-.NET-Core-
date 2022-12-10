452400cb6ae64292147d3a7d592a3e883920c3c2
Swathi Bhat <bhatswathi2221@gmail.com>
2022-12-10 23:49:19 +0530

    in Index.cshtml created a partial view called _Notification to display the tempdata which is added in category controller class and also created the functionality for the edit and delete.

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyBookWeb.Data;
 using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBookWeb.Controllers
 {
@@ -45,9 +46,78 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Add(category);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully!!";
                 return RedirectToAction("Index");
             }
             return View(category);
         }
+        //GET
+        public IActionResult Edit(int ? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            var objcategory = _db.Categories.Find(id);
+            //var categoryFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFirst = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if(objcategory == null)
+            {
+                return NotFound();
+            }
+            return View(objcategory);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplaySeq.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The name and displayseq should not be same");//server side validation
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully!!";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var objcategory = _db.Categories.Find(id);
+            //var categoryFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFirst = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (objcategory == null)
+            {
+                return NotFound();
+            }
+            return View(objcategory);
+        }
+        //POST
+        [HttpPost]//,ActionName("Delete")] //excplict way for giving the method name
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+               var objcategory = _db.Categories.Find(id);
+            if (objcategory == null )
+            {
+                return NotFound();
+            }
+         
+
+            _db.Categories.Remove(objcategory);
+                _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully!!";
+            return RedirectToAction("Index");
+           
+         }
     }
 }
