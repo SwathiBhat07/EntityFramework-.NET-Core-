c66600840d2bfc83716cbf3fc682ae09303916db
Swathi Bhat <bhatswathi2221@gmail.com>
2022-12-10 22:37:26 +0530

    added class annotations in Category.cs

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
@@ -9,6 +10,8 @@ namespace BulkyBookWeb.Models
         public int Id { get; set; }  //Property 
         [Required]// this tells the sql that name is required property and it should not be null.
         public String Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(10,1000,ErrorMessage ="Display order should between 10 to 1000 only")]
         public int DisplaySeq { get; set; }
         public DateTime Createddatetime { get; set; } = DateTime.Now;
 
